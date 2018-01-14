44815801e22a27adcc35f4f925b2d459965c52b0
Vincent Dubois <dubois.vct@free.fr>
2018-01-14 17:37:13 +0100

    First version

--- src/typescript-inject.spec.ts
+++ src/typescript-inject.spec.ts
@@ -1,6 +1,125 @@
+import { inject, InversionOfControlContainer, register } from './typescript-inject';
+
+const inversionOfControlContainer = InversionOfControlContainer.getInstance();
+
+class A {
+    method(): string {
+        return 'value from class A';
+    }
+}
+
 describe('Typescript Dependency Injection library', () => {
+
+    beforeEach(() => {
+        inversionOfControlContainer['clearInstances'].call(inversionOfControlContainer);
+    });
 
     describe('register function', () => {
 
+        it('should register a string value', done => {
+            // GIVEN
+
+            // WHEN
+            register('MyString', 'MyValue');
+
+            // THEN
+            expect(inversionOfControlContainer['instances']).toEqual([{
+                instanceName: 'MyString',
+                instanceValue: 'MyValue',
+                isSingleton: true
+            }]);
+            done();
+        });
+
+        it('should register a factory', done => {
+            // GIVEN
+            const myFactory = () => new A();
+
+            // WHEN
+            register('MyFactory', myFactory);
+
+            // THEN
+            expect(inversionOfControlContainer['instances']).toEqual([{
+                instanceName: 'MyFactory',
+                instanceValue: myFactory,
+                isSingleton: true
+            }]);
+            done();
+        });
+    });
+
+    describe('inject function', () => {
+
+        it('should throw an error if there is no instance named as requested', done => {
+            // GIVEN
+
+            // WHEN
+            try {
+                const valueToInject = inject<string>('StringToInjectThatDoesNotExist');
+                fail('we should not reach here because an error should have been thrown');
+                done();
+            } catch (exception) {
+                // THEN
+                expect(exception).not.toBeNull();
+                expect(exception.message).toEqual('An instance with a name such as \'StringToInjectThatDoesNotExist\' does not exist');
+                done();
+            }
+        });
+
+        it('should throw an error if there is a similar name of instance', done => {
+            // GIVEN
+            inversionOfControlContainer['instances'] = [{
+                instanceName: 'MyStringToInject',
+                instanceValue: 'My string value to inject',
+                isSingleton: true
+            }];
+
+            // WHEN
+            try {
+                const valueToInject = inject<string>('MyStringToinject');
+                fail('we should not reach here because an error should have been thrown');
+                done();
+            } catch (exception) {
+                // THEN
+                expect(exception).not.toBeNull();
+                expect(exception.message).toEqual(
+                    'An instance with a name such as \'MyStringToInject\' exists. Did you mispelled \'MyStringToinject\' ?');
+                done();
+            }
+        });
+
+        it('should inject a string value', done => {
+            // GIVEN
+            inversionOfControlContainer['instances'] = [{
+                instanceName: 'MyStringToInject',
+                instanceValue: 'My string value to inject',
+                isSingleton: true
+            }];
+
+            // WHEN
+            const injectedString = inject<string>('MyStringToInject');
+
+            // THEN
+            expect(injectedString).toEqual('My string value to inject');
+            done();
+        });
+
+        it('should inject a factory', done => {
+            // GIVEN
+            const factory = () => new A();
+            inversionOfControlContainer['instances'] = [{
+                instanceName: 'MyClassA',
+                instanceValue: factory,
+                isSingleton: true
+            }];
+
+            // WHEN
+            const injectedClass = inject<A>('MyClassA');
+
+            // THEN
+            expect(injectedClass).not.toBeNull();
+            expect(injectedClass.method()).toEqual('value from class A');
+            done();
+        });
     });
 });
---
+++ src/typescript-inject.ts
@@ -0,0 +1,69 @@
+interface Instance {
+    instanceName: string;
+    instanceValue: string | Function;
+    isSingleton: boolean;
+}
+
+export class InversionOfControlContainer {
+
+    static container: InversionOfControlContainer;
+    private instances: Array<Instance> = [];
+
+    private constructor() {
+
+    }
+
+    static getInstance() {
+        if (!this.container) {
+            this.container = new InversionOfControlContainer();
+        }
+        return this.container;
+    }
+
+    private clearInstances() {
+        this.instances = [];
+    }
+}
+
+export function register(instanceName: string, instanceValue: string | Function, isSingleton: boolean = true): void {
+    InversionOfControlContainer.getInstance()['instances'].push({
+        instanceName: instanceName,
+        instanceValue: instanceValue,
+        isSingleton: isSingleton
+    });
+}
+
+function instanceExists(instanceName: string) {
+    const instances = InversionOfControlContainer.getInstance()['instances'];
+    return instances.some(instance => instance.instanceName === instanceName);
+}
+
+function instanceWithSimilarNameExists(instanceName: string) {
+    const instances = InversionOfControlContainer.getInstance()['instances'];
+    return instances.some(instance => instance.instanceName.toUpperCase() === instanceName.toUpperCase());
+}
+
+function getInstanceWithSimilarNameExists(instanceName: string) {
+    const instances = InversionOfControlContainer.getInstance()['instances'];
+    return instances.find(instance => instance.instanceName.toUpperCase() === instanceName.toUpperCase());
+}
+
+function getInstanceWithName(instanceName: string): any {
+    const instances = InversionOfControlContainer.getInstance()['instances'];
+    let instance = instances.find(instance => instance.instanceName === instanceName).instanceValue;
+    if (typeof instance === 'function') {
+        return instance.call(instance);
+    }
+    return instance;
+}
+
+export function inject<T>(instanceName: string): T {
+    if (instanceExists(instanceName)) {
+        return getInstanceWithName(instanceName);
+    } else if (instanceWithSimilarNameExists(instanceName)) {
+        const instanceWithSimilarName = getInstanceWithSimilarNameExists(instanceName);
+        throw new Error(
+            `An instance with a name such as '${instanceWithSimilarName.instanceName}' exists. Did you mispelled '${instanceName}' ?`);
+    }
+    throw new Error(`An instance with a name such as '${instanceName}' does not exist`);
+}
